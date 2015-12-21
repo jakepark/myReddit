eb9886ce9dbe0ff690998e3a8f3d964190577102
jake park <jake.park@caa.columbia.edu>
2015-12-21 00:23:16 -0500

    posts factory initialize array

--- myreddit-rails/app/assets/javascripts/posts/posts.js
+++ myreddit-rails/app/assets/javascripts/posts/posts.js
@@ -4,14 +4,7 @@ angularServices.factory('posts', [
   '$http',
   function($http){
   var o = {
-    // posts: [
-    //   {author: 'user1', title: 'post 1', score: 5, date: 1, comments: []},
-    //   {author: 'user2', title: 'post 2', score: 2, date: 2, comments: []},
-    //   {author: 'user3', title: 'post 3', score: 15, date: 3, comments: []},
-    //   {author: 'user4', title: 'post 4', score: 9, date: 4, comments: []},
-    //   {author: 'user5', title: 'post 5', score: 4, date: 5, comments: []},
-    //   {author: 'user6', title: 'post 6', score: 0, date: 6, comments: []}
-    // ]
+    posts: []
   }
 
   o.getAll = function () {
