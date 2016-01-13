f6b71d5bed3c365a8c79358941be3945c1ce6d2d
jake park <jake.park@caa.columbia.edu>
2016-01-13 00:26:40 -0500

    $compile

--- myreddit-rails/app/assets/javascripts/home/mainCtrl.js
+++ myreddit-rails/app/assets/javascripts/home/mainCtrl.js
@@ -6,7 +6,7 @@ angularControllers.controller('MainCtrl', [
     '$scope',
     'posts',
     function($scope, posts) {
-
+      $scope.button_count = 0;
       $scope.posts = posts.posts
 
       $scope.addPost = function(){
@@ -30,3 +30,21 @@ angularControllers.controller('MainCtrl', [
     }
 
   ]);
+
+angularControllers.directive("addnotesbutton", function(){
+  return {
+    restrict: "E",
+    template: "<button addbuttons>Click to add buttons</button>"
+  }
+});
+
+angularControllers.directive("addbuttons", function($compile){
+  return function(scope, element, attrs){
+    element.bind("click", function(){
+      
+      scope.button_count++;
+      angular.element(document.getElementById('space-for-buttons'))
+        .append($compile("<div><button class='btn btn-default' data-alert="+scope.button_count+">Show alert #"+scope.button_count+"</button></div>")(scope));
+    });
+  };
+});
